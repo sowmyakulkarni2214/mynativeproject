23490c9e056e53dcdaa183e3ee7e250b0c1a7c75
SowmyaArivani <sowmya@arivani.net>
2025-01-09 15:07:36 +0530

    notification

--- api/baseUrlConfig.ts
+++ api/baseUrlConfig.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const instance = axios.create({ baseURL: "http://192.168.1.21:8080/api" })
+const instance = axios.create({ baseURL: "http://192.168.1.16:8080/api" })
 
 export { instance }
