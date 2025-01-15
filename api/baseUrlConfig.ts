41300de3bc26ef212a77ecfd1a870d85fed48bb3
SowmyaArivani <sowmya@arivani.net>
2025-01-15 22:31:23 +0530

    updated

--- api/baseUrlConfig.ts
+++ api/baseUrlConfig.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const instance = axios.create({ baseURL: "http://192.168.1.16:8080/api" })
+// const instance = axios.create({ baseURL: "http://192.168.1.16:8080/api" })
+const instance = axios.create({ baseURL: "https://myprojectbackend-cb85.onrender.com/api" })
+
 
 export { instance }
